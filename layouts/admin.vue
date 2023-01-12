2943f70c3edb623c8c4bd8c9599cab9add94cdfb
al-alec <yehadjialexis@gmail.com>
2023-01-12 03:50:35 +0000

    v1

---
+++ layouts/admin.vue
@@ -0,0 +1,120 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <div style="display: flex; flex-direction: column; align-items: center; margin-top: 10px; margin-bottom: 20px">
+        <div style="display: flex; gap: 5px; width: 75%; align-items: center;">
+          <div class="w-25">
+            <v-img src="/icon.png"/>
+          </div>
+          <span class="font-weight-bold text-h4">LOGO</span>
+        </div>
+        <span class="text-grey">Lorem ipsum dolor asit mer</span>
+      </div>
+
+      <div style="height: 80vh">
+        <div class="px-3" style="display: flex; flex-direction: column; justify-content: space-between; height: 100%">
+          <v-list nav density="compact">
+            <v-divider/>
+            <v-list-item
+                v-for="(item, i) in items"
+                :key="i"
+                :value="item"
+                active-color="primary"
+                rounded="shaped">
+              <template v-slot:prepend>
+                <v-icon :icon="item.icon"></v-icon>
+              </template>
+
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item>
+            <v-divider/>
+            <v-list-group value="Users">
+              <template v-slot:activator="{ props }">
+                <v-list-item
+                    v-bind="props"
+                    prepend-icon="mdi-account-circle"
+                    title="Users"
+                ></v-list-item>
+              </template>
+
+              <v-list-item
+                  v-for="i in 3"
+                  title="Admin"
+                  prepend-icon="mdi-account-circle"
+              ></v-list-item>
+            </v-list-group>
+            <v-list-group value="Users1">
+              <template v-slot:activator="{ props }">
+                <v-list-item
+                    v-bind="props"
+                    prepend-icon="mdi-account-circle"
+                    title="Users"
+                ></v-list-item>
+              </template>
+
+              <v-list-item
+                  v-for="i in 3"
+                  title="Admin"
+                  prepend-icon="mdi-account-circle"
+              ></v-list-item>
+            </v-list-group>
+            <v-divider/>
+          </v-list>
+
+          <v-card
+              color="secondary"
+              dark
+              class="mx-auto"
+              width="100%">
+            <div class="d-flex py-3 justify-space-between">
+              <v-list-item
+                  density="compact"
+                  prepend-icon="mdi-cog"
+              >
+                <v-list-item-subtitle>Paramètres</v-list-item-subtitle>
+              </v-list-item>
+            </div>
+
+          </v-card>
+        </div>
+      </div>
+    </v-navigation-drawer>
+
+    <v-app-bar app elevation="1" color="primary">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-items style="height: fit-content; margin-right: 25px">
+        <v-menu width="250px" >
+          <template v-slot:activator="{ props }">
+            <v-avatar style="cursor: pointer;" v-bind="props" color="white" >sk</v-avatar>
+          </template>
+          <v-list style="margin-top: 5px;">
+            <v-list-item>
+              <v-list-item-title>Profil</v-list-item-title>
+            </v-list-item>
+            <v-divider/>
+            <v-list-item prepend-icon="mdi-logout">
+              <v-list-item-title>Déconnexion</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main style="background-color: #f6f6f6">
+      <slot/>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+const drawer = ref(null)
+
+const selectedItem = ref(null)
+const items = [
+  {text: 'Real-Time', icon: 'mdi-clock'},
+  {text: 'Audience', icon: 'mdi-account'},
+  {text: 'Conversions', icon: 'mdi-flag'},
+]
+</script>
---
+++ pages/admin/dashboard.vue
@@ -0,0 +1,112 @@
+<template>
+  <NuxtLayout name="admin">
+    <v-container>
+      <v-row justify="center" class="space">
+        <v-col cols="9">
+          <v-row>
+            <v-col v-for="i in 3">
+              <v-card
+                  class="d-flex align-center rounded-xl"
+                  dark
+                  height="200"
+              >
+                <v-row>
+                  <v-col cols="12" sm="12">
+                    <v-list-item three-line class="mt-4">
+                      <div class="mb-4">
+                        <v-icon size="50px" color="primary">mdi-user</v-icon>
+                      </div>
+                      <v-list-item-subtitle class="text-h6 mb-2">
+                        Team velocity
+                      </v-list-item-subtitle>
+                      <v-list-item-title class="headline mb-1">
+                        <strong style="font-size: 25px;">52</strong>
+                      </v-list-item-title>
+                    </v-list-item>
+                  </v-col>
+                </v-row>
+
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="8">
+              <client-only>
+                <v-card>
+                  <GChart
+                      type="ColumnChart"
+                      :data="ColumnChartchartData"
+                      :options="ColumnChartchartOptions"
+                  />
+                </v-card>
+              </client-only>
+            </v-col>
+            <v-col cols="4">
+              <client-only>
+                <v-card>
+                  <GChart
+                      type="PieChart"
+                      :data="PieChartchartData"
+                      :options="PieChartchartOptions"
+                  />
+                </v-card>
+              </client-only>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="3">
+          <v-card height="80vh" style="overflow-y: auto">
+            <v-card-title style="position: sticky; top: 0px; z-index: 100; background-color: white">
+              Activités récents
+            </v-card-title>
+            <v-card-text>
+              <v-list lines="one">
+                <v-list-item
+                    v-for="item in 36"
+                    :rounded="true"
+                    :key="item"
+                    :title="'Lorem ipsum'"
+                    subtitle="lorem ipsum asit dolor set amer"
+                    :border="true"
+                    :prepend-avatar="'https://images.unsplash.com/photo-1671725501830-71073ba7eafc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'"
+                ></v-list-item>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </NuxtLayout>
+</template>
+<script setup lang="ts">
+ import {GChart} from 'vue-google-charts'
+
+const ColumnChartchartData = ref([
+  ['Year', 'Sales', 'Expenses', 'Profit'],
+  ['2014', 1000, 400, 200],
+  ['2015', 1170, 460, 250],
+  ['2016', 660, 1120, 300],
+  ['2017', 1030, 540, 350]
+])
+const ColumnChartchartOptions = ref({
+  title: 'Résumé des crédits, collectes & dépots',
+  subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+  height: '400',
+})
+
+
+ const PieChartdata = ref([
+   ['Task', 'Hours per Day'],
+   ['Work', 11],
+   ['Eat', 2],
+   ['Commute', 2],
+   ['Watch TV', 2],
+   ['Sleep', 7],
+ ]);
+
+ const PieChartoptions = ref({
+   title: 'My Daily Activities',
+   width: 800,
+   height: 600,
+ });
+</script>
